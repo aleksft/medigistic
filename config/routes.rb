73b4a3b1ed317e9ea2ce6f59621988aad104ebf0
Alejandra Fernandez <alexftelle@gmail.com>
2017-05-06 13:59:12 +0200

    Fix bug in root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Medigistic::Application.routes.draw do
-  root "home/index"
+  root 'home#index'
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
