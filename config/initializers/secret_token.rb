d3e1defeb78ee084df500148e2a2d6d70f161f27
Alejandra Fernandez <alexftelle@gmail.com>
2017-05-06 13:45:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medigistic::Application.config.secret_key_base = '82707ba3810f7ff36a93b09af4a6f72d05dbf275d782cf83f5b9b65f834d2d764b047916cb92a5c340399b22a213c9d2467e61027e19256a11e53164da1cf321'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Medigistic::Application.config.session_store :cookie_store, key: '_medigistic_session'
